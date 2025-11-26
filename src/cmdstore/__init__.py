58d4193b6116ba58061123e6cc7fd30357f13046
JakeerC <jakeerchilakala@gmail.com>
2025-11-26 11:45:56 +0530

    chore: Bump cmdstore package version to 0.6.0.

--- src/cmdstore/__init__.py
+++ src/cmdstore/__init__.py
@@ -5,6 +5,6 @@ cmdstore - A CLI tool to store and retrieve commands with fuzzy finding
 from cmdstore.cli import main
 from cmdstore.store import CommandStore
 
-__version__ = "0.4.0"
+__version__ = "0.6.0"
 
 __all__ = ["CommandStore", "main"]
